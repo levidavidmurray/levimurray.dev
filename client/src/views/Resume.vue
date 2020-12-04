d3c006b68ea01e6ed43cbb1507b8f1d889bd39bb
Levi Murray <levidavidmurray@gmail.com>
2020-12-04 15:12:36 -0800

    Updated resume style & position

--- client/src/views/Resume.vue
+++ client/src/views/Resume.vue
@@ -1,15 +1,15 @@
 <template>
   <app-page>
     <div class="app-content">
-      <img class="hero-image" src="../assets/images/blue-steel.jpeg" alt="">
-    </div>
-    <div class="resume">
-      <div class="overlay" @click="downloadResume">
-        <ion-button @click="downloadResume">
-          <ion-icon :icon="downloadOutline"></ion-icon>
-        </ion-button>
+      <img class="hero-image abs" src="../assets/images/blue-steel.jpeg" alt="">
+      <div class="resume">
+        <div class="overlay" @click="downloadResume">
+          <ion-button @click="downloadResume">
+            <ion-icon :icon="downloadOutline"></ion-icon>
+          </ion-button>
+        </div>
+        <img src="../assets/images/resume.png" alt="">
       </div>
-      <img src="../assets/images/resume.png" alt="">
     </div>
   </app-page>
 </template>
@@ -47,19 +47,27 @@ export default defineComponent({
 
 @keyframes slide-in {
   0% {
-    transform: translate3d(0,-500px,0);
+    transform: translate3d(0,0,0);
   }
   100% {
-    transform: translate3d(0,-300px,0);
+    transform: translate3d(0,100px,0);
   }
 }
 
 .resume {
-  width: fit-content;
-  margin: 0 auto;
+  width: 600px;
+  height: 776px;
+  margin-left: -24px;
   position: relative;
-  transform: translate3d(0,-500px,0);
-  animation: slide-in 3s ease forwards;
+  transform: translate3d(0,0,0);
+  animation: slide-in 1.75s ease forwards;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+
+  img {
+    width: 100%;
+  }
 
   .overlay {
     position: absolute;
